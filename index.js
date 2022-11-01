31722e9994a34abdbdbe811f0f0ab0e0ba8432ab
yukairex <wyktju@gmail.com>
2022-11-01 11:49:39 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const TOKEN = process.env.DISCORD_TOKEN;
+const CHANNELID = process.env.DISCORD_CHANNEL
+
+
+client.login(TOKEN);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+
+// const App = async () => {
+//     // setup discord client
+  
+//     await client.login(TOKEN);
+//     let channel = await client.channels.fetch(discordChannelId); //;
+
+//     channel.send("hello");
+
+// }
+
+// App()
